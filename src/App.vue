84c20a8740b7313303ada3422a4ea72a009d5e07
JS <csdnum0@gmail.com>
2022-11-22 21:21:38 +0900

    D

---
+++ src/App.vue
@@ -0,0 +1,293 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import LoveButton from './components/LoveButton.vue'
+$(document).ready(function () {
+  $('.content').click(function () {
+    $('.content').toggleClass("heart-active")
+    $('.text').toggleClass("heart-active")
+    $('.numb').toggleClass("heart-active")
+    $('.heart').toggleClass("heart-active")
+  });
+});
+</script>
+
+<template>
+  <div class="container-fluid text-center">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <section>
+        <!-- <br>
+        <br> -->
+        <!-- <div class="col"> -->
+          <LoveButton />
+        <!-- </div> -->
+        <!-- <br>
+        <br> -->
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-fluid {
+  background-color: #f3f3f3;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  overflow: hidden;
+}
+
+section {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: url(./resources/bg0.jpg);
+  background-position-x: center;
+  background-size: cover;
+  animation: animateBg 50s linear infinite;
+  /* z-index: -1; */
+}
+
+@keyframes animateBg {
+
+  0%,
+  100% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.2);
+  }
+}
+
+span {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 4px;
+  height: 4px;
+  background: #fff;
+  border-radius: 50%;
+  box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.1), 0 0 0 8px rgba(255, 255, 255, 0.1), 0 0 20px rgba(255, 255, 255, 1);
+  animation: animate 3s linear infinite;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 300px;
+  height: 1px;
+  background: linear-gradient(90deg, #fff, transparent);
+}
+
+@keyframes animate {
+  0% {
+    transform: rotate(315deg) translateX(0);
+    opacity: 1;
+  }
+
+  70% {
+    opacity: 1;
+
+  }
+
+  100% {
+    transform: rotate(315deg) translateX(-1500px);
+    opacity: 0;
+
+  }
+
+}
+
+span:nth-child(1) {
+  top: 0;
+  right: 0;
+  left: initial;
+  animation-delay: 0;
+  animation-duration: 1s;
+}
+
+span:nth-child(2) {
+  top: 0;
+  right: 80px;
+  left: initial;
+  animation-delay: 0.2s;
+  animation-duration: 3s;
+}
+
+span:nth-child(3) {
+  top: 80px;
+  right: 0px;
+  left: initial;
+  animation-delay: 0.4s;
+  animation-duration: 2s;
+}
+
+span:nth-child(4) {
+  top: 0;
+  right: 180px;
+  left: initial;
+  animation-delay: 0.6s;
+  animation-duration: 1.5s;
+}
+
+span:nth-child(5) {
+  top: 0;
+  right: 400px;
+  left: initial;
+  animation-delay: 0.8s;
+  animation-duration: 2.5s;
+}
+
+span:nth-child(6) {
+  top: 0;
+  right: 600px;
+  left: initial;
+  animation-delay: 1s;
+  animation-duration: 3s;
+}
+
+span:nth-child(7) {
+  top: 300px;
+  right: 0px;
+  left: initial;
+  animation-delay: 1s;
+  animation-duration: 1.75s;
+}
+
+span:nth-child(8) {
+  top: 0px;
+  right: 700px;
+  left: initial;
+  animation-delay: 1.4s;
+  animation-duration: 1.25s;
+}
+
+span:nth-child(9) {
+  top: 0px;
+  right: 1000px;
+  left: initial;
+  animation-delay: 0.75s;
+  animation-duration: 2.25s;
+}
+
+span:nth-child(10) {
+  top: 0px;
+  right: 1000px;
+  left: initial;
+  animation-delay: 2.75s;
+  animation-duration: 2.25s;
+}
+
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+a:hover {
+  color: #535bf2;
+}
+
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+a:hover {
+  color: #535bf2;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+button:hover {
+  border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
+.card {
+  padding: 2em;
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+@media (prefers-color-scheme: light) {
+  :root {
+    color: #213547;
+    background-color: #ffffff;
+  }
+  a:hover {
+    color: #747bff;
+  }
+  button {
+    background-color: #f9f9f9;
+  }
+}
+</style>
---
+++ src/components/LoveButton.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="col">
+    <br>
+    <br>
+    <h1 style="color:white">슬기♥승동 "{{remainTime}}" 축하해!♥</h1>
+    <br>
+    <br>
+    <div style="position:relative">
+      <div class="heart-btn">
+        <div class="content">
+          <!-- <span class="heart"></span>
+          <span class="text"></span> -->
+          <span class="numb"></span>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <img src="../resources/jjidol1.jpg" class="rounded" alt="..." style="width:100px;height:100px">
+    <img src="../resources/nana1.jpg" class="rounded" alt="..." style="width:100px;height:100px">
+    <img src="../resources/nana-jjidol1.jpg" class="rounded" alt="..." style="width:100px;height:100px">
+    <br>
+    <br>
+  </div>
+  <div class="col">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item" data-bs-interval="3000">
+
+            <img src="../resources/love1.jpg" class="rounded" alt="..." style="width:50%;height:50%">
+            <rect width="100%" height="100%" fill="#777"></rect>
+
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+          <img src="../resources/love2.jpg" class="rounded" alt="..." style="width:50%;height:50%">
+
+            <rect width="100%" height="100%" fill="#666"></rect>
+
+        </div>
+        <div class="carousel-item active" data-bs-interval="3000">
+          <img src="../resources/love3.jpg" class="rounded" alt="..." style="width:50%;height:50%">
+
+            <rect width="100%" height="100%" fill="#555"></rect>
+
+
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+  <br>
+  <br>
+
+</template>
+<script>
+import {ref, onMounted} from 'vue'
+
+export default {
+  setup () {
+    const remainTime = ref('');
+    const countDown = ref(10)
+    const getDate = () => {
+      setTimeout(() => {
+        const masTime = new Date('2021-03-03 00:00:00');
+        const todayTime = new Date();
+      
+        const diff = todayTime - masTime;
+
+        const diffDay = (Math.floor(diff / (1000*60*60*24)));
+        const diffHour = Math.floor((diff / (1000*60*60)) % 24);
+        const diffMin = Math.floor((diff / (1000*60)) % 60);
+        const diffSec = Math.floor(diff / 1000 % 60);
+      
+        // const remainTime = `${diffDay}일`;
+        // console.log('jsdno0 debug1', diffDay)
+        remainTime.value = `${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`;
+        // console.log('jsdno0 debug2', remainTime.value)
+        // if(diffDay < 600){
+          getDate()
+        // }
+      }, 1000);
+    }
+    const countDownTimer = () => {
+      // console.log('jsdno0 debug2', countDown.value)
+                if (countDown.value > 0) {
+                    setTimeout(() => {
+                      console.log('jsdno0 debug1', countDown.value)
+                      countDown.value -= 1
+                        countDownTimer()
+                    }, 1000)
+                }
+            }
+    onMounted(()=>{
+      // console.log('jsdno0 debug3', countDown.value)
+      getDate()
+    })
+
+    return {
+      remainTime,
+      getDate,
+      countDown,
+      countDownTimer
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:600&display=swap');
+
+.read-the-docs {
+  color: #888;
+}
+
+.heart-btn {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.content {
+  padding: 13px 16px;
+  display: flex;
+  border: 2px solid #eae2e1;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.content.heart-active {
+  border-color: #f9b9c4;
+  background: #fbd0d8;
+}
+
+
+.heart {
+  position: absolute;
+  /*background: url("../resources/img.png") no-repeat;*/
+  background-position: left;
+  background-size: 2900%;
+  height: 90px;
+  width: 90px;
+  top: 50%;
+  left: 21%;
+  transform: translate(-50%, -50%);
+}
+
+.text {
+  font-size: 21px;
+  margin-left: 30px;
+  color: grey;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.numb:before {
+  content: '♡ I Love You';
+  font-size: 21px;
+  margin-left: 7px;
+  font-weight: 600;
+  color: #9C9496;
+  font-family: sans-serif;
+}
+
+.numb.heart-active:before {
+  content: '♥ Forever';
+  color: #000;
+}
+
+.text.heart-active {
+  color: #000;
+}
+
+.heart.heart-active {
+  animation: animate .8s steps(28) 1;
+  background-position: right;
+}
+
+@keyframes animate {
+  0% {
+    background-position: left;
+  }
+
+  100% {
+    background-position: right;
+  }
+}
+</style>
